48b1c149ae282eb1f3ba583514dfabe1c5787896
theophilousmapiye28 <theophilousmapiye28@gmail.com>
2024-11-05 10:38:00 +0200

    new changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -65,6 +65,7 @@ const Contact = () => {
             <h5 className="text-xl font-bold text-white my-2">Lets Connect</h5>
             <p className="text-[#ADB7BE] mb-4 max-w-md">
               I m currently looking for new opportunities. My inbox is always open. Whether you have a question or just want to say hi, Ill try my best to get back to you!
+              .....
             </p>
             <div className="socials flex flex-row gap-2">
               <Link href="https://github.com/theophilousmapiye28">
